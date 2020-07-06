28017bff6630ae26ec9418f33af3a0f9ff8912be
Yoshitaka Terazawa <locol6622@gmail.com>
2020-07-06 17:02:11 +0900

    chore: change npm to yarn

--- packages/hooks-use-context-base/src/components/Foo.tsx
+++ packages/hooks-use-context-base/src/components/Foo.tsx
@@ -1,9 +1,22 @@
 import React, { useContext } from 'react'
 import { Context } from './context'
+import styled from 'styled-components'
+
+const lists = ['blue', 'red', 'green']
 
 export const Foo = () => {
   const context = useContext(Context)
 
-  return <div>{context.text}</div>
+  return (
+    <>
+      <div>{context.text}</div>
+      {lists.map((color, index) => (
+        <Item key={`${color}-${index}`} color={color} />
+      ))}
+    </>
+  )
 }
 
+const Item = styled.li({}, ({ color }) => ({
+  backgroundColor: `${color}`,
+}))
