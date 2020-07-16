c18cbe17a6e0735f3b54cd87aea624cf51a81390
Yoshitaka Terazawa <locol6622@gmail.com>
2020-07-16 09:30:44 +0900

    feat: remove unnecessary code

--- packages/hooks-use-context-base/src/components/Foo.tsx
+++ packages/hooks-use-context-base/src/components/Foo.tsx
@@ -1,22 +1,9 @@
 import React, { useContext } from 'react'
 import { Context } from './context'
-import styled from 'styled-components'
-
-const lists = ['blue', 'red', 'green']
 
 export const Foo = () => {
   const context = useContext(Context)
 
-  return (
-    <>
-      <div>{context.text}</div>
-      {lists.map((color, index) => (
-        <Item key={`${color}-${index}`} color={color} />
-      ))}
-    </>
-  )
+  return <div>{context.text}</div>
 }
 
-const Item = styled.li({}, ({ color }) => ({
-  backgroundColor: `${color}`,
-}))
