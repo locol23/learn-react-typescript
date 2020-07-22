c82d1782326d368097b77e39e5e48512b04a508a
Yoshitaka Terazawa <locol6622@gmail.com>
2020-07-22 18:30:42 +0900

    feat: update render-props-base

--- packages/render-props-base/src/components/App.tsx
+++ packages/render-props-base/src/components/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Child } from './Child'
 
-export const App: React.FC = () => {
-  const render = () => <div>render props</div>
-
-  return (
-    <>
-      <Child render={render} />
-      <Child />
-    </>
-  )
+type Props = {
+  render?: () => React.ReactNode
 }
+
+const Child: React.FC<Props> = ({ render }) => <>{render ? render() : <p>render is nothing</p>}</>
+
+export const App: React.FC = () => (
+  <>
+    <Child render={() => <div>render props</div>} />
+    <Child />
+  </>
+)
+
--- packages/render-props-base/src/components/Child.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react'
-
-type Props = {
-  render?: () => ReactNode
-}
-
-export const Child: React.FC<Props> = ({ render }) => <>{render ? render() : <p>render is nothing</p>}</>
-
